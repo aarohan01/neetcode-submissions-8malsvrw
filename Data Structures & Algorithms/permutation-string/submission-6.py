692b0a1d7ec1d2d408151cb50cb7cc7fb67ce058
Rohan Hasabnis <rohan.hasabnis.in@gmail.com>
2026-03-31 13:35:29 -0700

    Add: permutation-string - submission-6

---
+++ Data Structures & Algorithms/permutation-string/submission-6.py
@@ -0,0 +1,87 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+
+
+        ### Bruteforce ###
+        '''
+        sortedS1 = ''.join(sorted(s1))
+        lenS1 = len(s1)
+        print(f'S1 : {sortedS1}')
+        L = 0
+        for R in range(lenS1,len(s2)+1):
+
+            window = ''.join(sorted(s2[L:R]))
+            print(f'w - {window}')
+            if window == sortedS1:
+                return True
+            L += 1
+        return False
+        '''
+
+
+        ### Frequency Table + Sliding window ###
+        # Use freq table to compare windows
+        # This works but is not the most efficient due to slicing and then calculating freq for each window again 
+        # n ( for s1) + n*m (for s2), similarly every slice will use memory 
+        # Time : O(nm)
+        # Space : O(1)
+        ## Better version below ##
+        '''
+        def frequency(s: str) -> List:
+
+            table = [0]*26
+            for letter in s:
+                table[ord(letter)-ord('a')] += 1
+            return table
+
+        s1table = frequency(s1)
+        print(s1table)
+
+        L=0
+        for R in range(len(s1),len(s2)+1):
+            s2table = frequency(s2[L:R])
+            if s1table == s2table:
+                return True
+            L += 1
+        return False
+        '''
+
+
+
+        ### Rolling freq table + sliding window ###
+        s1table, s2table = [0]*26, [0]*26
+        
+
+        # S1 table 
+        for l in s1:
+            s1table[ord(l)-ord('a')] += 1
+        print(s1table)
+        
+        L=0
+        for R in range(len(s1),len(s2)+1):
+            
+            if L == 0:
+                for l in s2[L:R]:
+                    s2table[ord(l)-ord('a')] += 1
+                print(s2table)
+            
+            else:
+                s2table[ord(s2[R-1])-ord('a')] += 1
+
+
+            if s1table == s2table:
+                return True
+
+            s2table[ord(s2[L])-ord('a')] -= 1
+            L += 1
+        return False
+        
+
+
+
+
+    
+
+ 
+
+
