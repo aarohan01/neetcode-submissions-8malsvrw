286bca94d943edc89b64b3261a165b596772da88
Rohan Hasabnis <rohan.hasabnis.in@gmail.com>
2026-03-31 12:30:44 -0700

    Add: minimum-stack - submission-11

---
+++ Data Structures & Algorithms/minimum-stack/submission-11.py
@@ -0,0 +1,74 @@
+
+### Two Stack ###
+# Use two stacks, one maintains value and one maintains minvalue
+# While pushing just check if stack empty or less than previous min value and insert in minstack accordingly
+# Time : O(1)
+# Space : O(n)
+'''
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minstack = []
+
+    def push(self, val: int) -> None:
+        
+        if not self.stack or val < self.minstack[-1]:
+            self.minstack.append(val)
+        else:
+            self.minstack.append(self.minstack[-1])
+        self.stack.append(val)
+
+
+    def pop(self) -> None:
+
+        self.stack.pop()
+        self.minstack.pop()
+        
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        return self.minstack[-1]
+'''
+### One Stack Trick ###
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minval = None
+
+    def push(self, val: int) -> None:
+        
+        # If empty
+        if not self.stack:
+            self.minval = val
+
+        self.stack.append(val - self.minval)
+
+        # After every push update minval 
+        self.minval = min(val,self.minval)
+
+
+    def pop(self) -> None:
+        
+        val = self.stack[-1]
+        if val < 0:
+            self.minval -= val
+        self.stack.pop()
+        
+
+    def top(self) -> int:
+        
+        val = self.stack[-1]
+        if val > 0:
+            return val + self.minval
+        else:
+            return self.minval
+        
+
+    def getMin(self) -> int:
+        return self.minval
+        
