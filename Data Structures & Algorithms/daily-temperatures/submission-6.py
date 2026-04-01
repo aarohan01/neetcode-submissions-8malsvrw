aa8a9743b23abc41efed0e61a7509096fe525ee3
Rohan Hasabnis <rohan.hasabnis.in@gmail.com>
2026-03-31 21:32:12 -0700

    Add: daily-temperatures - submission-6

---
+++ Data Structures & Algorithms/daily-temperatures/submission-6.py
@@ -0,0 +1,39 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+
+        
+        ### Bruteforce ###
+        # Time : O(n)
+        # Space : O(1)
+        '''
+        res = [0]*len(temperatures)
+        for i in range(len(temperatures)):
+            for j in range(i+1,len(temperatures)):
+
+                if temperatures[j] > temperatures[i]:
+                    res[i] = j-i
+                    break
+        return res
+        '''
+
+        ### Stack ###
+        # Next greater hence stack is best option 
+        # Its running difference - If a higher temp is detected that means for whatever is in the stack currently we can 
+        # count days instead of calculaling for each individually , until stack is in desc order we just move on the for loop 
+        # appending elements, when detected we pop and store diff and continue the for loop for remaining elements that means 
+        # all elements are visited only once.
+
+        res = [0]*len(temperatures)
+        stack = []
+        for i in range(len(temperatures)):
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                    j = stack.pop()
+                    res[j] = i - j
+                    
+            stack.append(i)
+        return res
+
+
+
+
+        
