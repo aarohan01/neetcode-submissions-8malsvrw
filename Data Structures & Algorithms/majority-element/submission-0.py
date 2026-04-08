19989702168f57bb329928282d5e6900a2443a40
Rohan Hasabnis <rohan.hasabnis.in@gmail.com>
2026-04-07 18:59:44 -0700

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        
+        ### Bruteforce ###
+        hashmap = {}
+        maxfreq = 0
+        maxfreqnum = None
+        for i in nums:
+
+            hashmap[i] =  hashmap.get(i,0) + 1
+            if hashmap[i] > maxfreq:
+                maxfreq = hashmap[i]
+                maxfreqnum = i
+
+        return maxfreqnum
