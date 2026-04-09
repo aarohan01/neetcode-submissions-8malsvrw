067cb60c7c82d7fab93b48a2832cbc5917077699
Rohan Hasabnis <rohan.hasabnis.in@gmail.com>
2026-04-08 17:47:39 -0700

    Add: find-duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-2.py
@@ -0,0 +1,51 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        
+
+        ### Bruteforce ###
+        # Using Hashset 
+        # Time : O(n)
+        # Space : O(n)
+        '''
+        hashset = set()
+
+        for i in nums:
+
+            if i in hashset:
+                return i
+            hashset.add(i)
+        '''
+
+        ### Fast and Slow +  Slow and Slow ###
+        '''
+        curfast, curslow = nums[0], nums[0]
+        while True:
+            curfast = nums[nums[curfast]]
+            curslow = nums[curslow]
+            if curfast == curslow:
+                break
+        print(curfast)
+
+        newslow = nums[0]
+        while True:
+            if curslow == newslow:
+                return curslow
+            curslow = nums[curslow]
+            newslow = nums[newslow]
+        '''
+
+
+        curfast, curslow = 0, 0
+        while True:
+            curfast = nums[nums[curfast]]
+            curslow = nums[curslow]
+            if curfast == curslow:
+                break
+        print(curfast)
+
+        newslow = 0
+        while True:
+            curslow = nums[curslow]
+            newslow = nums[newslow]
+            if curslow == newslow:
+                return curslow
