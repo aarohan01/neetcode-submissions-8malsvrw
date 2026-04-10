fe56fdfe5b84d4545dd487ee6fa4b057d270322a
Rohan Hasabnis <rohan.hasabnis.in@gmail.com>
2026-04-10 16:22:40 -0700

    Add: reorder-linked-list - submission-3

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-3.py
@@ -0,0 +1,53 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+
+        
+        ### Floyds Slow and Fast + Slow and Slow ###
+        ## Find mid reverse from mid, Use a new slow pointers to reorder ##
+        ## Use the example given to figure out : [2,4,6,8] ##
+
+
+        curslow, curfast = head, head 
+
+        # Find midpoint
+        while curfast and curfast.next:
+            curfast = curfast.next.next
+            curslow = curslow.next
+        
+        print(curslow.val)
+        #print(curfast.val)
+
+        # Reverse from mid
+        prev = None
+        while curslow:
+            temp = curslow.next
+            curslow.next = prev
+            prev = curslow
+            curslow = temp
+        
+        
+        # Reconstruct the linked list
+        newslow, curslow = head, prev
+        while curslow.next:
+
+            tmp1 = newslow.next
+            newslow.next = curslow
+            newslow = tmp1
+
+            tmp2 = curslow.next 
+            curslow.next = newslow
+            curslow = tmp2
+        
+            
+            
+
+
+
+
+
