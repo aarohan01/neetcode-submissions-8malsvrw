2274c45ab4b7453c012ceac8371ef00d92a46067
Rohan Hasabnis <rohan.hasabnis.in@gmail.com>
2026-04-09 23:23:42 -0700

    Add: reverse-a-linked-list - submission-9

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-9.py
@@ -0,0 +1,50 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+
+        ### Bruteforce ###
+        # Traverse and put values in array 
+        # Reconstruct the linked list in reverse order of array
+        # Time: O(n)
+        # Space : O(n)
+        '''
+        cur = head
+        arr = []
+        while cur:
+            arr.append(cur.val)
+            cur = cur.next
+
+        print(arr)
+
+        head = ListNode()
+        cur = head
+        for i in range(len(arr)-1,-1,-1):
+
+            cur.next = ListNode(arr[i])
+            cur = cur.next
+
+        return head.next
+        '''
+
+        ### Inplace ###
+        # Use two pointer prev and cur, prev is None and cur is at current Head
+        # Reverse using a temp variable
+        # Time: O(n)
+        # Space: O(1)
+        prev, cur = None, head
+
+        while cur:
+            temp = cur.next
+            cur.next = prev
+            prev = cur
+            cur = temp
+        
+        return prev
+
+        
