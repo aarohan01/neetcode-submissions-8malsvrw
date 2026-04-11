cdbdaa28cf46c892f429d385fc92eb8d90bc4ad9
Rohan Hasabnis <rohan.hasabnis.in@gmail.com>
2026-04-10 19:28:13 -0700

    Add: majority-element - submission-10

---
+++ Data Structures & Algorithms/majority-element/submission-10.py
@@ -0,0 +1,46 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        
+        ### Bruteforce 1 ###
+        # For every number count it if count is  len(nums) // 2 return that number instantly
+        # Time : O(n^2)
+        # Space : O(1)
+        '''
+        for i in range(len(nums)):
+            count = 0
+            for j in range(len(nums)):
+                if nums[i] == nums[j]:
+                    count += 1
+                    if count >= len(nums) // 2:
+                        return nums[i]
+        '''
+
+
+        ### Moore's voting algo ###
+        # 1. Set result whenever count is at 0 and increment.
+        # 2. Other times increase count if new number same as result 
+        # 3. Else decrease count if new number not same as result
+        count = 0
+        res = None 
+        for n in nums:
+            
+            if count == 0:
+                count += 1
+                res = n
+                continue
+            
+            if n == res:
+                count += 1
+            else:
+                count -= 1
+
+        return res
+            
+
+
+
+
+
+
+
+
