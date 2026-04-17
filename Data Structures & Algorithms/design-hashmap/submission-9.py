d0831ecd97082f931480ede32dc239185ac437b5
Rohan Hasabnis <rohan.hasabnis.in@gmail.com>
2026-04-17 01:46:24 -0700

    Add: design-hashmap - submission-9

---
+++ Data Structures & Algorithms/design-hashmap/submission-9.py
@@ -0,0 +1,78 @@
+class ListNode:
+
+    # For chaining
+    def __init__(self, key, val, next=None):
+        self.key = key
+        self.val = val 
+        self.next = next
+
+class MyHashMap:
+
+    def __init__(self):
+        self.hashmap = [None]*1000
+
+    def put(self, key: int, value: int) -> None:
+        
+        bucket = key % 1000
+
+        # Nothing exist        
+        if self.hashmap[bucket] == None:
+            self.hashmap[bucket] = ListNode(key,value)
+            return 
+        
+        # Check value or insert node
+        cur = self.hashmap[bucket]
+        while cur:
+            if cur.key == key:
+                cur.val = value
+                break
+            
+            if cur.next is None:
+                cur.next = ListNode(key,value)
+                break
+            
+            cur = cur.next
+
+    def get(self, key: int) -> int:
+
+
+        bucket = key % 1000
+        if self.hashmap[bucket] == None:
+            return -1
+        
+        cur = self.hashmap[bucket]
+        while cur:
+            if cur.key == key:
+                return cur.val
+            cur = cur.next
+        return -1
+
+
+        
+
+    def remove(self, key: int) -> None:
+
+        bucket = key % 1000
+        if self.hashmap[bucket] is not None:
+            cur = self.hashmap[bucket]
+            if cur.key == key:
+                self.hashmap[bucket] = cur.next
+                return
+            else:
+                while cur.next:
+                    if cur.next.key == key:
+                        cur.next = cur.next.next
+                        return 
+                    cur = cur.next
+            
+
+            
+
+        
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
