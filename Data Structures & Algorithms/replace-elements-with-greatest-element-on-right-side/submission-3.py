d7bc7285e14d75422aac2335503aaf0f5ff167e1
Rohan Hasabnis <rohan.hasabnis.in@gmail.com>
2026-04-17 01:00:07 -0700

    Add: replace-elements-with-greatest-element-on-right-side - submission-3

---
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-3.py
@@ -0,0 +1,33 @@
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        
+        ### Bruteforce ###
+        # Time: O(n^2)
+        # Space: O(1)
+        '''
+        for i in range(len(arr)-1):
+            maxnum = float('-inf')
+            for j in range(i+1,len(arr)):
+
+                maxnum =  max(maxnum, arr[j])
+
+            arr[i] = maxnum
+        arr[-1] = -1
+        return arr
+        '''
+
+        ### Suffix/ Right Max ###
+        # From right onwards keep calculating the max and and replacing, but store the current number
+        # to update max as we go 
+        # Time: O(n)
+        # Space: O(1)
+        maxnum = arr[-1]
+        for i in range(len(arr)-2,-1,-1):
+
+            curnum = arr[i]
+            arr[i] = maxnum
+            maxnum = max(maxnum, curnum)
+        
+        arr[-1] = -1
+        return arr
+
