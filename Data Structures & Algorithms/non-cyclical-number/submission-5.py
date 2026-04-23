6c3781dc8c5e251b532a5080733458e00a52848b
Rohan Hasabnis <rohan.hasabnis.in@gmail.com>
2026-04-22 17:13:06 -0700

    Add: non-cyclical-number - submission-5

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-5.py
@@ -0,0 +1,62 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        
+        
+        ### Bruteforce ###
+        # Using a hashset to see if number appeared already if yes return False
+        # If at any stage number becomes 1 return True
+        # Time: O(logn)
+        # Space: O(logn)
+        '''
+        hashset = set()
+        
+        while n != 1:
+            total = 0
+            while n:
+                total += (n % 10)**2
+                n = n // 10
+            
+            print(total)
+            if total in hashset:
+                return False
+            hashset.add(total)
+            n = total
+        return True
+        '''
+
+        ### Floyd's fast and slow pointers ###
+        # If we draw the results of each addition and read the question "non-cyclical"
+        # That means if we detect a cycle in this linked list it is False
+        # If instead we find value 1 then true
+        # Time : O(logn)
+        # Space : O(1)
+
+        ### Returns next number in the linked list i.e. next number after n ###
+        def nextNumber(n:int) -> int:
+        
+            total = 0
+            while n:
+                total += (n % 10)**2
+                n = n // 10
+            return total
+        
+        
+        # For number n let curslow be next num and curfast is next to next
+        # If curfast or curslow is 1 at any stage its non-cyclical return true
+        # If curfast and curslow meet anywhere there is a cycle thus return false
+        curfast, curslow = nextNumber(nextNumber(n)), nextNumber(n)
+        print(curfast,curslow)
+        while curfast != 1 or curslow != 1:
+
+            if curfast == curslow:
+                return False
+            
+            curfast, curslow = nextNumber(nextNumber(curfast)), nextNumber(curslow)
+        
+        return True
+            
+            
+
+
+
+                
