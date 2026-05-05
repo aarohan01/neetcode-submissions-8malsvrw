bed62c321a47dec9560fda55d503726734a1d6d5
Rohan Hasabnis <rohan.hasabnis.in@gmail.com>
2026-05-04 21:29:02 -0700

    Add: missing-number - submission-4

---
+++ Data Structures & Algorithms/missing-number/submission-4.py
@@ -0,0 +1,28 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        
+        ### Bruteforce ###
+
+        for i in range(len(nums)+1):
+
+            if i not in nums:
+                return i
+
+        ### Bruteforce - sorting ###
+        # Sort and check diff 
+        # Time: O(nlogn)
+        # Space: O(1)
+        '''
+        nums.sort()
+        if nums[0] != 0:
+            return 0
+        if nums[-1] != len(nums):
+            return len(nums)
+
+        for i in range(len(nums)-1):
+
+            if nums[i+1]-nums[i] != 1:
+                return nums[i]+1
+        '''
+
+        
