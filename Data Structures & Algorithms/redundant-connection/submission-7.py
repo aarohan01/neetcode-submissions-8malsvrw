6869c18d23f9e463f38767c7627b6e51f1369f12
Rohan Hasabnis <rohan.hasabnis.in@gmail.com>
2026-07-05 00:27:30 -0700

    Add: redundant-connection - submission-7

---
+++ Data Structures & Algorithms/redundant-connection/submission-7.py
@@ -0,0 +1,215 @@
+class UnionFind:
+
+    def __init__(self,n):
+        
+        self.components = n
+        self.parents = {}
+        self.ranks = {}
+
+        for i in range(1,n+1):
+            self.parents[i] = i
+            self.ranks[i] = 0
+        print(self.parents)
+    
+    def find(self, node):
+
+        cur = self.parents[node]
+
+        while cur != self.parents[cur]:
+            
+            # Optimization 
+            self.parents[cur] = self.parents[self.parents[cur]]
+            # Move up
+            cur = self.parents[cur]
+        
+        return cur
+
+
+    def union(self, n1, n2):
+
+        root_n1, root_n2 = self.find(n1), self.find(n2)
+
+        if root_n1 == root_n2:
+            return True
+        
+        if self.ranks[root_n1] > self.ranks[root_n2]:
+            self.parents[root_n2] = root_n1
+        elif self.ranks[root_n1] < self.ranks[root_n2]:
+            self.parents[root_n1] = root_n2
+        else:
+            self.parents[root_n2] = root_n1
+            self.ranks[root_n1] += 1
+        
+        self.components -= 1
+
+        return False
+
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        
+
+        ### DFS normal coverage multicell + skip parent cycle detection ###
+        ## Since undirected and where ever there is cycle its extra edge.
+        ## Only one extra edge
+        ### THIS CODE CORRECTLY DETECTS CYCLE AND GIVES THE EDGE ###
+        ## But the issue is that it gives the edge in order of nodes ##
+        # The question expects the edge that causes cycle to be in the order or the edge list #
+        # Example : Edgelist : [[1,2],[1,3],[2,3]] 
+        # According to this if we use adj list to construct graph 
+        # We will go from 1 - 2 - 3 - 1 and detect cycle and return [1,3] as cycle edge 
+        # But the question expects [2,3] because if we used edge list it would be 1-2 then 1-3 and 2-3 causing cycle
+        # So in reality any edge removal is sufficient but answer is expected in order of edge list
+        ##############################################################
+        '''
+
+        # Converting edge list to adj list for DFS
+        adj = {}
+
+        for x,y in edges:
+            
+            adj[x] = adj.get(x,[])
+            adj[y] = adj.get(y,[])
+
+            # Undirected thus both ways
+            adj[x].append(y)
+            adj[y].append(x)
+
+        print(adj)
+
+        visited = set()
+        cycle_nodes = []
+        def dfs(node,parent):
+            
+            # Cycle
+            if node in visited:
+                cycle_nodes.append(node)
+                cycle_nodes.append(parent)
+                return True
+
+
+            visited.add(node)
+
+            for nei in adj[node]:
+
+                ### Skip parent ###
+                if nei == parent:
+                    continue
+                
+                if dfs(nei, node):
+                    return True
+
+            return False
+
+        
+        for node in adj:
+            print(node)
+            if node not in visited:
+                if dfs(node,None):
+                    return cycle_nodes
+        '''
+
+
+        ### DFS normal coverage multicell + skip parent ###
+        # In order to give the proper edge of the cycle 
+        # When cycle is detected in adj list 
+        # record the cylcle
+        # Then loop the edgelist in reverse to get the edge
+        # Time: O(V+E) -> making adj list , dfs
+        # Space: O(V+E) -> adj list
+
+        
+        # Converting edge list to adj list for DFS
+        '''
+        adj = {}
+
+        for x,y in edges:
+            
+            adj[x] = adj.get(x,[])
+            adj[y] = adj.get(y,[])
+
+            # Undirected thus both ways
+            adj[x].append(y)
+            adj[y].append(x)
+
+        print(adj)
+
+        
+        visited = set()
+        cycle = set()
+        cycle_start = None
+        def dfs(node,parent):
+            
+            nonlocal cycle_start
+            # Cycle
+            if node in visited:
+                cycle_start = node
+                return True
+
+
+            visited.add(node)
+
+            for nei in adj[node]:
+
+                ### Skip parent ###
+                if nei == parent:
+                    continue
+                
+                if dfs(nei, node):
+
+                    if cycle_start:
+                        cycle.add(node)
+                    
+                    if node == cycle_start:
+                        cycle_start = None
+
+                    return True
+
+            return False
+
+        # DFS on the graph and stop if cycle, for loop since can be disconnected thus multicell DFS
+        for node in adj:
+            print(node)
+            if node not in visited:
+                if dfs(node,None):
+                    break 
+        
+        # Reverse the edge list for searching last edge causing cycle
+        for u,v in edges[::-1]:
+            # Cannot use u == cycle_start as cycle start was set to None
+            if u in cycle and v in cycle:
+                return [u,v]
+
+        '''
+
+
+
+
+
+
+
+        ### Union Find cycle detection ###
+        ## Edge list given and we want edge that causes cycle - Union Find 
+        # Build Union Find and apply union from edge list
+        # When cycle is detected return the edge
+        # Time: O(V) + O(E * α(V)) -> Build Tree + Union
+        # Space: O(V) -> Tree
+ 
+
+        
+        # Create UnionFind DS
+        n = len(edges)
+        uf = UnionFind(n)
+
+        for u,v in edges:
+            print(u,v)
+            if uf.union(u,v):
+                return [u,v]
+        
+
+            
+
+
+
+
+            
