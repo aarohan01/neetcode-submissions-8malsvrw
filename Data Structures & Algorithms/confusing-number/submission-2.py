7c74f24a32b14525fee41602331af9bbb43d5e20
Rohan Hasabnis <rohan.hasabnis.in@gmail.com>
2026-07-10 12:20:32 -0700

    Add: confusing-number - submission-2

---
+++ Data Structures & Algorithms/confusing-number/submission-2.py
@@ -0,0 +1,32 @@
+class Solution:
+    def confusingNumber(self, n: int) -> bool:
+
+
+        number = n
+        res = []
+        
+        if n == 0:
+            return False
+
+        while n:
+
+            digit = n % 10
+            if digit in [2,3,4,5,7]:
+                return False
+            
+            elif digit in [0,1,8]:
+                res.append(str(digit))
+            
+            elif digit == 6:
+                res.append('9')
+            else:
+                res.append('6')
+
+            n = n // 10
+
+
+        print(res)
+        if int(''.join(res)) == number:
+            return False
+
+        return True
